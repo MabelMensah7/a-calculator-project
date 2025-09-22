21bb1cfb73c85e83834e6e43b6e035ffd4aadf84
MabelMensah7 <mabelmensah515@gmail.com>
2025-09-22 19:19:32 +0000

    This is a calculator

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Serve static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "result": ""})
+
+@app.post("/calculate", response_class=HTMLResponse)
+def calculate(request: Request, num1: float = Form(...), num2: float = Form(...), operation: str = Form(...)):
+    result = None
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        result = num1 / num2 if num2 != 0 else "Cannot divide by 0"
+    return templates.TemplateResponse("index.html", {"request": request, "result": result})
